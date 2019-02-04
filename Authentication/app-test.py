bfe1661d2b52a670a67dc946cff408a0fcbd9c1f
sirrah23 <hseth93@gmail.com>
2019-02-04 01:22:04 -0500

    Install necessary libraries and create a hello world endpoint

---
+++ Authentication/app-test.py
@@ -0,0 +1,15 @@
+from app import app
+
+import unittest
+
+
+class HelloWorldUnitTest(unittest.TestCase):
+
+    def test_hello_world_returns_the_good_stuff(self):
+        request, response = app.test_client.get('/')
+        assert response.status == 200
+        assert response.json.get('hello') == 'world'
+
+if __name__ == '__main__':
+    unittest.main()
+    
