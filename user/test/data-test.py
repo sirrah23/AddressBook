a5a2def3037dfca734e0f9a926eeaaf3cc813795
sirrah23 <hseth93@gmail.com>
2019-02-21 23:01:13 -0500

    Add tests for the UserDataObject

---
+++ user/test/data-test.py
@@ -0,0 +1,45 @@
+from user.user.data import UserDataObject
+
+import unittest
+
+class CreateUDOTest(unittest.TestCase):
+
+    def test_create_udo_success(self):
+
+        udo = UserDataObject("username", "password", "email@domain.com")
+        assert udo.username == "username"
+        assert udo.password != "password"
+        assert udo.email == "email@domain.com"
+
+        # Password should be encrypted
+        assert type(udo.password) == bytes
+
+        # Auto-generated UUID
+        assert len(udo.uuid) > 0
+
+    def test_create_from_data_dict_success(self):
+        data_dict = {"username": "username", "password": b"password", "email":
+                "email@domain.com", "uuid": "1234-5678-91011"}
+        udo = UserDataObject.from_data_dict(data_dict)
+        assert udo.uuid == data_dict["uuid"]
+        assert udo.username == data_dict["username"]
+        assert udo.password == data_dict["password"]
+        assert udo.email == data_dict["email"]
+
+    def test_create_udo_fail(self):
+        with self.assertRaises(TypeError):
+            udo = UserDataObject(1, 2, 3)
+
+class UDODataDictDumpTest(unittest.TestCase):
+
+    def test_data_dict_dump_udo(self):
+        udo = UserDataObject("username", "password", "email@domain.com")
+        dd = udo.to_data_dict()
+        assert dd is not None
+        assert "uuid" in dd
+        assert "username" in dd
+        assert "password" in dd
+        assert "email" in dd
+
+if __name__ == "__main__":
+    unittest.main()
