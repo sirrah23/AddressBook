057cfe73b10b8deb9c6bb0e6fa80c8ea49368c64
sirrah23 <hseth93@gmail.com>
2019-02-28 01:01:52 -0500

    Create a new ValidationService object
    The purpose of this object is to validate that a given (username,
    password) pair is valid. The results will be sent back to the caller.
    
    We can wrap this logic in a `/validate` endpoint eventually.

--- user/user/service.py
+++ user/user/service.py
@@ -1,5 +1,10 @@
+import bcrypt
+
+# TODO: Services could inherit from a BaseService class
+
 class RegistrationService:
-    """A service that can be used to register new users by adding them to the system.
+    """A service that can be used to register new users by adding them to the
+    system.
     """
 
     def __init__(self, user_sql, user_data_obj):
@@ -22,6 +27,7 @@ class RegistrationService:
         
         return {"error": 0, "errorMsg": "", "user": udo.to_data_dict()}
     
+    # TODO: Change name of this function to be consistent with the ValidationService
     def is_valid_register_params(self, rp):
         needed_parameters = ["username", "password", "email"]
         return all(p in rp for p in needed_parameters)
@@ -31,3 +37,29 @@ class RegistrationService:
         if res["user"]:
             res["user"].pop("password", None)
         return res
+
+
+class ValidationService:
+    """A service that can be used to validate a given set of credentials e.g.
+    (username, password)
+    """
+
+    def __init__(self, user_sql, user_data_obj):
+        self.user_sql = user_sql()
+        self.user_data_obj = user_data_obj
+
+    def is_valid_params(self, rp):
+        needed_parameters = ["username", "password"]
+        return all(p in rp for p in needed_parameters)
+
+    def validate_user(self, rp):
+        res = {"found": False, "user_id": None, "username": None}
+        user = self.user_sql.fetch_by_username(rp["username"])
+        if not user:
+            return res
+        found = bcrypt.checkpw(rp["password"], user.password)
+        res["found"] = found
+        res["user_id"] = user.uuid
+        res["username"] = user.username
+        return res
+
