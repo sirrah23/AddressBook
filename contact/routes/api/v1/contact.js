f8460d73b610f5d4189844b41dfb9e8cdf277f1d
sirrah23 <hseth93@gmail.com>
2019-05-07 00:20:54 -0400

    Modify put and delete contact to be more RESTful
    For these endpoints I was deleting/updating a single contact but the url
    didn't have the contactId as a parameter. For REST endpoints that act on
    a single entity that is supposed to be how it's done, hence this change.

--- contact/routes/api/v1/contact.js
+++ contact/routes/api/v1/contact.js
@@ -25,9 +25,10 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.put('/', async(req, res) => {
+router.put('/:contactId', async(req, res) => {
     logger.info(`Start contact put with parameters: ${JSON.stringify(req.body)}`)
     const userUUID = req.body.userUUID
+    const contactId = req.params.contactId
     const contact = req.body.contact
     if(!userUUID){
         logger.warn('Aborting, a user uuid was not found, check auth middleware')
@@ -37,13 +38,11 @@ router.put('/', async(req, res) => {
         logger.warn('Aborting, the caller did not supply any contact data')
         return res.status(400).json({errorFlag: 1, message: "No contact data provided", contact: {}})
     }
-    if(!contact.id){
+    if(!contactId){
         logger.warn('Aborting, the caller did not supply a contact id')
         return res.status(400).json({errorFlag: 1, message: "No contact id provided", contact: {}})
     }
-    const data = Object.assign({}, contact)
-    delete data.id
-    const contactCreateRes = await ContactController.updateContact(userUUID, contact.id, data)
+    const contactCreateRes = await ContactController.updateContact(userUUID, contactId, contact)
     if (contactCreateRes.errorFlag === 0){
         logger.info(`Success: ${contactCreateRes.message}`)
         return res.status(200).json(contactCreateRes)
@@ -53,23 +52,19 @@ router.put('/', async(req, res) => {
     }
 })
 
-router.delete('/', async(req, res) => {
+router.delete('/:contactId', async(req, res) => {
     logger.info(`Start contact delete with parameters: ${JSON.stringify(req.body)}`)
     const userUUID = req.body.userUUID
-    const contact = req.body.contact
+    const contactId = req.params.contactId
     if(!userUUID){
         logger.warn('Aborting, a user uuid was not found, check auth middleware')
         return res.status(400).json({errorFlag: 1, message: "Invalid user authorization", contact: {}})
     }
-    if(!contact){
-        logger.warn('Aborting, the caller did not supply any contact data')
-        return res.status(400).json({errorFlag: 1, message: "No contact data provided", contact: {}})
-    }
-    if(!contact.id){
+    if(!contactId){
         logger.warn('Aborting, the caller did not supply a contact id')
         return res.status(400).json({errorFlag: 1, message: "No contact id provided", contact: {}})
     }
-    const contactDeleteRes = await ContactController.deleteContact(userUUID, contact.id)
+    const contactDeleteRes = await ContactController.deleteContact(userUUID, contactId)
     if (contactDeleteRes.errorFlag === 0){
         logger.info(`Success: ${contactDeleteRes.message}`)
         return res.status(200).json(contactDeleteRes)
