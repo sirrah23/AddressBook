813db4dc02a1a36ad9331f3d4b610f021e668433
sirrah23 <hseth93@gmail.com>
2019-05-07 00:05:35 -0400

    Turn off logging when tests are running

--- contact/util/logger.js
+++ contact/util/logger.js
@@ -11,9 +11,14 @@ const logger = createLogger({
       format.json()
     ),
     defaultMeta: { service: 'contact' },
-    transports: [
-      new transports.Console(),
-    ]
+    transports: []
   });
-  
+
+if(process.env.mode === "test"){
+  //Don't want to see log messages when tests are running...
+  logger.add(new transports.File({filename: '/dev/null'})) 
+} else {
+  logger.add(new transports.Console())
+}
+
 module.exports = logger
