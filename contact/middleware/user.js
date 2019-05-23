b7df9ed91c4c63a8bc634898fbaa98edb54a83f3
sirrah23 <hseth93@gmail.com>
2019-05-23 00:10:31 -0400

    Add tests for userAddMiddleware

--- contact/middleware/user.js
+++ contact/middleware/user.js
@@ -3,13 +3,18 @@ const User = require('../model/user.js')
 async function userAddMiddleware(req, res, next){
     if(!req.authDataPayload)
         return res.status(500).json({errorFlag: 1, errorMsg: 'Something went wrong'})
-    const {uuid} = req.authDataPayload
+    const uuid = req.authDataPayload.user_id
     if(!uuid)
         return res.status(500).json({errorFlag: 1, errorMsg: 'Something went wrong'})
     let user = await User.fetch(uuid)
-    if(user !== null)
+    if(user !== null){
+        req.body.userUUID = user.uuid
         return next()
+    }
     user = new User(uuid)
     await user.save()
+    req.body.userUUID = user.uuid
     return next()
 }
+
+module.exports = userAddMiddleware
--- contact/test/middleware.test.js
+++ contact/test/middleware.test.js
@@ -2,6 +2,16 @@ const sinon = require('sinon')
 const httpMocks = require('node-mocks-http')
 const authorizationMiddleware = require('../middleware/authorizer.js')
 const authServiceConnMock = require('../mock/authServiceConnMock.js')
+const userAddMiddleware = require('../middleware/user.js')
+const User = require('../model/user.js')
+const wipeTables = require('../data/wipe.js')
+const dbConn = require('../data/dbConn.js')
+
+afterAll(async done => {
+    await wipeTables()
+    await dbConn.destroy() //TODO: Remove the need for this by properly cleaning up database connections
+    done()
+})
 
 test('succesfully authorize a request with a valid token', async done => {
     const testUsername = 'user001'
@@ -31,5 +41,40 @@ test('fail to validate an invalid token', async done => {
     expect(jsonResponse.errorFlag).toBe(1)
     expect(jsonResponse.errorMsg).toBe('Unauthorized request!')
     expect(nextSpy.calledOnce).toBe(false)
+    done()
+})
+
+test('succesfully add a user to the database via the contents of a successfully validated auth token payload', async done => {
+    const testUsername = 'user001'
+    const token = authServiceConnMock.generateAuthTokenRequest(testUsername)
+    const nextSpyAuth = sinon.spy()
+    const req = httpMocks.createRequest()
+    const res = httpMocks.createResponse()
+    req.headers.authorization = `bearer ${token}`
+    await authorizationMiddleware(req, res, nextSpyAuth)
+    const nextSpyUser = sinon.spy()
+    await userAddMiddleware(req, res, nextSpyUser)
+    expect(nextSpyUser.calledOnce).toBe(true)
+    expect(!!req.body.userUUID).toBe(true)
+    const user = await User.fetch(req.body.userUUID)
+    expect(user !== null).toBe(true)
+    expect(user.uuid).toBe(req.body.userUUID)
+    // expect(user.username).toBe(testUsername)
+    done()
+})
+
+test('successfully move on if the user already exists in the database', async done => {
+    const testUsername = 'user001'
+    const token = authServiceConnMock.generateAuthTokenRequest(testUsername)
+    const nextSpyAuth = sinon.spy()
+    const req = httpMocks.createRequest()
+    const res = httpMocks.createResponse()
+    req.headers.authorization = `bearer ${token}`
+    await authorizationMiddleware(req, res, nextSpyAuth)
+    const user = new User(req.authDataPayload.user_id)
+    user.save()
+    const nextSpyUser = sinon.spy()
+    await userAddMiddleware(req, res, nextSpyUser)
+    expect(nextSpyUser.calledOnce).toBe(true)
     done()
 })
