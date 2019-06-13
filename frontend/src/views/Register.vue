45e962f994c9d96ed8c2dbaf4f3e3b8170a71327
sirrah23 <hseth93@gmail.com>
2019-06-13 01:00:15 -0400

    Add registration page
    We now have an empty registration page, but it doesn't do anything yet.
    
    The next step is to take the data from the registration form and do some real work with it.

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Register</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    v-model="username"
+                    name="username"
+                    label="Username"
+                    type="text"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    id="password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="passwordConfirm"
+                    id="passwordConfirm"
+                    name="password"
+                    label="Confirm password"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn @click="register" color="primary">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    username: "",
+    password: "",
+    passwordConfirm: ""
+  }),
+
+  props: {
+    source: String
+  },
+
+  methods: {
+    register: function() {
+      //TODO
+    }
+  }
+};
+</script>
