d1259571ff2aa672e2e09ca72854d30d4593c427
sirrah23 <hseth93@gmail.com>
2019-07-11 01:28:01 -0400

    Add sample contact cards
    There are a series of cards on the main page with some fake data at the moment. Each of these cards is eventually going to house real contact data.

--- frontend/src/views/Book.vue
+++ frontend/src/views/Book.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="book">
-    <h1>Contacts</h1>
-  </div>
+  <v-app id="book">
+    <v-content>
+      <v-container grid-list-lg>
+        <v-layout row wrap>
+          <v-flex offset-md3 md6 v-for="i in 5" :key="`${i}`">
+            <v-card>
+              <v-card-title primary-title>
+                <div>
+                  <h3 class="headline mb-0">Contact {{i}}</h3>
+                  <div>{{ card_text }}</div>
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn flat color="orange">View</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
