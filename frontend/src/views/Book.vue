967b97403739b85227065760a766118973a3e890
sirrah23 <hseth93@gmail.com>
2019-07-11 01:35:27 -0400

    Redesign contact card
    I moved the "View" button to the right of the card and added some template data for email and phone number.
    
    Now that we have some idea of what the UI for this is going to look like, we can move the contact card logic into a separate component that can then be fed props for contact data.

--- frontend/src/views/Book.vue
+++ frontend/src/views/Book.vue
@@ -5,15 +5,24 @@
         <v-layout row wrap>
           <v-flex offset-md3 md6 v-for="i in 5" :key="`${i}`">
             <v-card>
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">Contact {{i}}</h3>
-                  <div>{{ card_text }}</div>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">View</v-btn>
-              </v-card-actions>
+              <v-container>
+                <v-layout row wrap>
+                  <v-card-title primary-title>
+                    <div>
+                      <h3 class="headline mb-0">Contact {{ i }}</h3>
+                      <div>Phone Number: 999-432-512{{ i }}</div>
+                      <div>Email: contact{{ i }}@system.com</div>
+                    </div>
+                  </v-card-title>
+                </v-layout>
+                <v-layout row wrap>
+                  <v-flex offset-md9>
+                    <v-card-actions>
+                      <v-btn flat color="orange">View</v-btn>
+                    </v-card-actions>
+                  </v-flex>
+                </v-layout>
+              </v-container>
             </v-card>
           </v-flex>
         </v-layout>
