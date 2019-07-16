448761e21c063002dd8ff3d1f6d6018afc39ea35
sirrah23 <hseth93@gmail.com>
2019-07-16 00:36:11 -0400

    Create a Contact component
    I moved the code for the Contact card into its own component. Now, when we finally fetch contact data from the backend, we can simply feed it into a series of Contact components.
    
    The Contact components will eventually have business logic to handle some scenarios like edits and deletes, but for now we don't need any of that.

--- frontend/src/views/Book.vue
+++ frontend/src/views/Book.vue
@@ -3,30 +3,25 @@
     <v-content>
       <v-container grid-list-lg>
         <v-layout row wrap>
-          <v-flex offset-md3 md6 v-for="i in 5" :key="`${i}`">
-            <v-card>
-              <v-container>
-                <v-layout row wrap>
-                  <v-card-title primary-title>
-                    <div>
-                      <h3 class="headline mb-0">Contact {{ i }}</h3>
-                      <div>Phone Number: 999-432-512{{ i }}</div>
-                      <div>Email: contact{{ i }}@system.com</div>
-                    </div>
-                  </v-card-title>
-                </v-layout>
-                <v-layout row wrap>
-                  <v-flex offset-md9>
-                    <v-card-actions>
-                      <v-btn flat color="orange">View</v-btn>
-                    </v-card-actions>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
-          </v-flex>
+          <Contact
+            v-for="i in 5"
+            :key="i"
+            :name="'Contact ' + i"
+            :email="'contact' + i + '@email.com'"
+            :phoneNumber="'999-776-123' + i"
+          />
         </v-layout>
       </v-container>
     </v-content>
   </v-app>
 </template>
+
+<script>
+import Contact from "../components/Contact";
+
+export default {
+  components: {
+    Contact
+  }
+};
+</script>
