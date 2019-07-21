cc99a06ba259d9e9ec2d08cebe8acef1b77fe600
sirrah23 <hseth93@gmail.com>
2019-07-20 20:34:13 -0400

    Add placeholder for logout dropdown

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <v-app>
+    <v-content>
+      <v-container>
+      <v-layout row wrap>
     <v-toolbar app>
+      <v-flex>
       <v-toolbar-title class="headline text-uppercase">
         <span>AddressBook</span>
       </v-toolbar-title>
+      </v-flex>
+      <v-flex v-if="username" offset-md10>
+          {{username}}
+      </v-flex>
     </v-toolbar>
+      </v-layout>
+    </v-container>
+    </v-content>
 
     <v-content>
       <router-view />
@@ -20,6 +31,11 @@ export default {
     return {
       //
     };
+  },
+  computed: {
+    username(){
+      return this.$store.getters["user/username"]
+    }
   }
 };
 </script>
