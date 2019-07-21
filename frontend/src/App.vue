efc85531477e196f4bfd817a450422938b5f0c60
sirrah23 <hseth93@gmail.com>
2019-07-20 20:42:01 -0400

    Add a dropdown with username/logout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,19 +2,30 @@
   <v-app>
     <v-content>
       <v-container>
-      <v-layout row wrap>
-    <v-toolbar app>
-      <v-flex>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>AddressBook</span>
-      </v-toolbar-title>
-      </v-flex>
-      <v-flex v-if="username" offset-md10>
-          {{username}}
-      </v-flex>
-    </v-toolbar>
-      </v-layout>
-    </v-container>
+        <v-layout row wrap>
+          <v-toolbar app>
+            <v-flex>
+              <v-toolbar-title class="headline text-uppercase">
+                <span>AddressBook</span>
+              </v-toolbar-title>
+            </v-flex>
+            <v-flex v-if="username" offset-md10>
+              <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                  <v-btn color="primary" dark v-on="on">
+                    {{ username }}
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-tile>
+                    <v-list-tile-title>Logout</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+            </v-flex>
+          </v-toolbar>
+        </v-layout>
+      </v-container>
     </v-content>
 
     <v-content>
@@ -33,8 +44,8 @@ export default {
     };
   },
   computed: {
-    username(){
-      return this.$store.getters["user/username"]
+    username() {
+      return this.$store.getters["user/username"];
     }
   }
 };
